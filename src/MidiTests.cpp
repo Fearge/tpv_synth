61288800fa3fcc23613e37f3485a228798dd04b1
Fearge <Fearge>
2023-10-23 15:08:35 +0200

    Skizze für MIDI Ansteuerung

--- src/MidiTests.cpp
+++ src/MidiTests.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include "MIDI.h";
+#include <synth.h>
 
 // Create a MIDI object
 MIDI_CREATE_DEFAULT_INSTANCE();
@@ -7,9 +8,17 @@ MIDI_CREATE_DEFAULT_INSTANCE();
 // Define the LED pin
 const int ledPin = 13;  // You can choose any digital pin
 
+//how many notes are pressed
+byte notesPressed;
+
+int voice;
+
+synth edgar;
+
 void setup() {
   MIDI.begin(MIDI_CHANNEL_OMNI);  // Initialize MIDI communication
   pinMode(ledPin, OUTPUT);       // Set the LED pin as an output
+  edgar.begin();
 }
 
 void loop() {
@@ -19,14 +28,43 @@ void loop() {
 
     // Check if it's a Note On message (status byte 0x90 and velocity > 0)
     if (status == 0x90 && MIDI.getData2() > 0) {
+      
+      //every note simultaneously pressed, takes 1 bit Space of the Byte
+      notesPressed = notesPressed << 1;
+      notesPressed++;
 
-        digitalWrite(ledPin, HIGH);  // Turn the LED on
+    //set voices
+    switch (notesPressed)
+    {
+    case 0x1:
+      voice = 0;
+      break;
+
+    case 0x3:
+    voice = 1;
+    break;
+
+    case 0x7:
+    voice = 2;
+    break;
+
+    case 0xF:
+    voice = 3;
+    break;
+    
+    default:
+      break;
+    }
+
+    //trigger synth with current Note
+      edgar.mTrigger(voice,MIDI.getData1());
       
     }
 
     // Check if it's a Note Off message (status byte 0x80 or velocity = 0)
     if ((status == 0x80) || (status == 0x90 && MIDI.getData2() == 0)) {
-        digitalWrite(ledPin, LOW);  // Turn the LED off
+        //make Space in Byte
+        notesPressed = notesPressed >> 1;
       
     }
   }
