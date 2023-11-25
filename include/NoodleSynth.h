034c36a4b732c281ef5182b19e2185810a91d496
Fearge <mario1.seibert@web.de>
2023-11-25 23:42:49 +0100

    ready for presentation

--- include/NoodleSynth.h
+++ include/NoodleSynth.h
@@ -3,6 +3,5 @@
 //A lot of libraries exist out there with the same name, so forcing the compiler to search here is user-friendly
 #include "Arduino.h"
 #include "synthEngine.h"  //These include statements are the only objects the User can create in their *.ino file
-#include "MusicWithoutDelay.h"
 
 #endif
